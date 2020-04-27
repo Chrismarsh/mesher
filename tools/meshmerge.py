d1e992a71f1b59d3c0cf34684512bc87bcb89ecd
Chris Marsh <chrismarsh.c2@gmail.com>
2020-04-27 11:36:49 -0600

    Merge multiple files into one

--- tools/meshmerge.py
+++ tools/meshmerge.py
@@ -1,48 +1,59 @@
 #!/usr/bin/env python
-
-import sys, os,pdb
 import json
-import numpy as np
+from pathlib import Path
 
 # Set up CL arguments
 import argparse
 parser = argparse.ArgumentParser()
 
 parser.add_argument("-o", "--outfile", required=False,
-                help="File for output (default overwrites reference file).")
+                help="File for output. Default is an filename from all input files.")
 
-parser.add_argument("-ref", "--reffile", required=True,
-                help="Reference file containing original mesh.")
-
-parser.add_argument("-i", "--infile", required=True,
-                help="File containing parameters to be added.")
-
-import matplotlib as mpl
-mpl.use('AGG')  # non-gui display (much faster)
-import matplotlib.pyplot as plt
+parser.add_argument("infile",
+                    nargs='+',
+                    help="Files containing parameters to be merged.")
 
 
 def append_new_param_to_file(args):
     """Read dictionary of arguments, find parameters, add new parameters to reference json file"""
 
-    with open(args["infile"]) as f:
-        param_in = json.load(f)
-      
-    with open(args["reffile"]) as f:
-        param_ref = json.load(f)
+    params = args["infile"]
 
-    # Add parameters to param_ref
-    param_ref.update(param_in)
+    # load the first to append to
+    with open(params[0]) as f:
+        param_merged = json.load( f )
+
+    for i in range(1, len(params)):
+        with open(params[i]) as f:
+            param_in = json.load(f)
+            # Add parameters to param_ref
+            param_merged.update(param_in)
+
+    lengths = []
+
+    print('Merged param file has:')
+    for param, data in param_merged.items():
+
+        l = len(data)
+        lengths.append(l)
+        print(F"{param}: {l}")
+
+    if len(set(lengths)) > 1:
+        print('ERROR: Not all parameters have the same length, which they must.')
 
     with open(args["outfile"],'w') as f:
-        json.dump(param_ref, f, indent=4)
+        json.dump(param_merged, f, indent=4)
 
 
 if __name__=="__main__":
 
     # Parse the input arguments
     args = vars(parser.parse_args())
-    if (args["outfile"]==None):
-        args["outfile"] = args["reffile"]
+    if args["outfile"] is None:
+        name = ''
+        for f in args["infile"]:
+            filename = Path(f).resolve().stem
+            name = name + filename + '+'
+        args["outfile"] = '%s.param' % name[:-1]
 
     append_new_param_to_file(args)
