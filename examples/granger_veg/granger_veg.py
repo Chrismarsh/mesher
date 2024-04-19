2d1868e9b9094015b1d460143c6780c24cbb81be
Chris Marsh <chrismarsh.c2@gmail.com>
2024-04-19 20:38:28 +0000

    Update example file

--- examples/granger_veg/granger_veg.py
+++ examples/granger_veg/granger_veg.py
@@ -21,12 +21,13 @@ max_tolerance = 1    # 5m maxe RMSE between triangle and underlying elevation se
 min_area = 5**2     #triangle area below which we will no longer refine, regardless of max_tolerance
 
 use_weights = True
-reuse_mesh=False
+reuse_mesh = False
 
 
 MPI_nworkers=1
 nworkers_gdal=1
 
+mesher_path = "/home/chm003/project-ords/code/mesher/build/build-bin/mesher"
 
 parameter_files = {
     'landcover': {'file': '../data/eosd.tif',
