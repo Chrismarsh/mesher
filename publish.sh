8672ebc3cb7dfbb1af28102f687959c782c6b226
Chris Marsh <chrismarsh.c2@gmail.com>
2024-05-12 16:35:26 -0600

    Add spack env yaml + generator lines in publish

--- publish.sh
+++ publish.sh
@@ -1,5 +1,7 @@
 set -e
-. ./venv-deploy/bin/activate
-python -m build
+# spack env create . py-mesher-spack.yaml
+spack env activate . --sh
+# spack install
+python -m build --sdist
 rm -f dist/*.whl # we don't want to upload these
-twine upload  dist/*
+twine upload  dist/* --skip-existing
