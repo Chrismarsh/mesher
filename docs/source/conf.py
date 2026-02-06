d9b59cd6794762195a825931a2e336b8c4119e48
Chris Marsh <chrismarsh.c2@gmail.com>
2026-02-06 21:10:07 +0000

    update doc looks

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,8 @@ author = 'Chris Marsh'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
- "sphinx_rtd_theme",
+    "sphinx_design",
+    "myst_nb",
  'sphinx.ext.autosectionlabel'
 ]
 
@@ -52,12 +53,28 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_book_theme'
+html_theme_options = {
+    "repository_url": "https://github.com/Chrismarsh/mesher",
+    "repository_branch": "develop",
+    "path_to_docs": "docs",
+    "use_edit_page_button": True,
+    "use_repository_button": True,
+    "use_issues_button": True,
+    "home_page_in_toc": False,
+    "logo": {
+        "image_light": "_static/images/mesher_logo.png",
+        "image_dark": "_static/images/mesher_logo.png",
+    },
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = [] # '_static'
+html_static_path = ['_static']
+html_css_files = [
+    "fix-notebook-darkmode.css",
+]
 
 html_theme_options = {
     'collapse_navigation': False,
